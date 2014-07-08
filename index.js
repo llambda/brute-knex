acc6aa862b8540d388d4ed9f0b71dc5fbb013e94
llambda <llambda@users.noreply.github.com>
2014-07-07 22:31:28 -0500

    Inital knex support

--- index.js
+++ index.js
@@ -1,63 +1,107 @@
 var AbstractClientStore = require('express-brute/lib/AbstractClientStore'),
-    Redis = require('redis'),
     _ = require('lodash'),
-    knex = require('knex')
+    sqlite3 = require('sqlite3')
 
-var RedisStore = module.exports = function (options) {
+var KnexStore = module.exports = function (options) {
+  var that = this;
 	AbstractClientStore.apply(this, arguments);
-	this.options = _.extend({}, RedisStore.defaults, options);
-	this.redisOptions = _(this.options).clone();
-	delete this.redisOptions.prefix;
-	delete this.redisOptions.client;
-	delete this.redisOptions.port;
-	delete this.redisOptions.host;
+	this.options = _.extend({}, KnexStore.defaults, options);
+	this.knexOptions = _(this.options).clone();
 
-	if (this.options.client) {
-		this.client = this.options.client;
-	} else {
-		this.client = RedisStore.Redis.createClient(
-			this.options.port,
-			this.options.host,
-			this.options.redisOptions
-		);
-	}
+  this.knex = require('knex')(this.options);
+  this.ready = this.knex.schema.hasTable(this.options.tablename)
+              .then(function (exists) {
+                if (!exists) {
+                   return that.knex.schema.createTable(that.options.tablename, function (table) {
+                    table.string('key')
+                    table.dateTime('firstRequest')
+                    table.dateTime('lastRequest')
+                    table.integer('lifetime')
+                    table.integer('count')
+                  })
+                } else {
+                  return true;
+                }
+              });
 };
-RedisStore.prototype = Object.create(AbstractClientStore.prototype);
-RedisStore.prototype.set = function (key, value, lifetime, callback) {
-	lifetime = parseInt(lifetime, 10) || 0;
-	var multi    = this.client.multi(),
-	    redisKey = this.options.prefix+key;
+KnexStore.prototype = Object.create(AbstractClientStore.prototype);
+KnexStore.prototype.set = function (key, value, lifetime, callback) {
+  var that = this;
+  lifetime = lifetime || 0;
 
-	multi.set(redisKey, JSON.stringify(value));
-	if (lifetime > 0) {
-		multi.expire(redisKey, lifetime);
-	}
-	multi.exec(function (err, data) {
-		typeof callback == 'function' && callback.call(this, null);
-	});
+  that.ready.then(function () {
+    that.knex.select('*').from(that.options.tablename).where('key', '=', key)
+    .then(function (foundKeys) {
+      if (foundKeys.length == 0) {
+        console.log(value)
+        return that.knex(that.options.tablename)
+        .insert({
+            key: key,
+            lifetime: lifetime,
+            lastRequest: value.lastRequest,
+            firstRequest: value.firstRequest,
+            count: value.count
+          })
+        .then(function () {
+          callback(null);
+        })
+        // .catch(function (err) {
+        //   callback(err);
+        // })
+      } else {
+        return that.knex(that.options.tablename)
+        .where('key', '=', key)
+        .update({
+           lifetime: lifetime,
+           count: value.count+foundKeys[0].count,
+           lastRequest: value.lastRequest
+          })
+        .then(function (x) {
+          callback.call(null);
+        })
+        // .catch(function (err) {
+        //   callback.call(err);
+        // })
+      }
+    })
+  })
 };
-RedisStore.prototype.get = function (key, callback) {
-	this.client.get(this.options.prefix+key, function (err, data) {
-		if (err) {
-			typeof callback == 'function' && callback(err, null);
-		} else {
-			if (data) {
-				data = JSON.parse(data);
-				data.lastRequest = new Date(data.lastRequest);
-				data.firstRequest = new Date(data.firstRequest);
-			}
-			typeof callback == 'function' && callback(err, data);
-		}
-	});
+KnexStore.prototype.get = function (key, callback) {
+  var that = this;
+  that.ready.then(function () {
+    return that.knex.select('*')
+    .from(that.options.tablename)
+    .where('key', '=', key)
+    .then(function (response) {
+      var data = {};
+      if (response[0]) {
+        data.lastRequest = new Date(response[0].lastRequest);
+        data.firstRequest = new Date(response[0].firstRequest);
+        data.count = response[0].count;
+        callback(null, data)
+      } else {
+        callback(null, {
+          count:0,
+          firstRequest:new Date(),
+          lastRequest:new Date()
+        });
+      }
+    })
+    // .catch(function (err) {
+    //   callback(err)
+    // })
+  })
 };
-RedisStore.prototype.reset = function (key, callback) {
+KnexStore.prototype.reset = function (key, callback) {
 	this.client.del(this.options.prefix+key, function (err, data) {
 		typeof callback == 'function' && callback.apply(this, arguments);
 	});
 };
-RedisStore.Redis = Redis;
-RedisStore.defaults = {
-	prefix: '',
-	port: 6379,
-	host: '127.0.0.1'
+KnexStore.defaults = {
+	client: 'sqlite3',
+  debug:true,
+  connection: {
+    filename: "./express-brute.sqlite"
+  },
+  tablename: 'brute'
 };

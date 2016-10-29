d0f3a35632320865bb19bdb1fe0185bb8d460e75
Grant Miner <llambda@users.noreply.github.com>
2016-10-29 10:49:00 -0500

    Tab indents

--- example.js
+++ example.js
@@ -7,16 +7,16 @@ var store = new BruteKnex({
 });
 
 const bruteforce = new ExpressBrute(store, {
-  freeRetries: 2
+	freeRetries: 2
 });
 
 const express = require('express');
 const app = express();
 app.get('/',
-  bruteforce.prevent, // 403 if we hit this route too often
-  function (req, res, next) {
-    res.send('Success!');
-  }
+	bruteforce.prevent, // 403 if we hit this route too often
+	function (req, res, next) {
+		res.send('Success!');
+	}
 );
 
 app.listen(3000);

652956b215b67a9a79acff32baab05db8303581d
kirrg001 <katharina.irrgang@googlemail.com>
2018-04-30 19:48:49 +0200

    Added eslint
    no issue
    
    - added some ES6

--- example.js
+++ example.js
@@ -1,22 +1,21 @@
 const ExpressBrute = require('express-brute');
 const BruteKnex = require('.');
-const Knex = require('knex');
 
-var store = new BruteKnex({
-	createTable: true
+let store = new BruteKnex({
+    createTable: true
 });
 
 const bruteforce = new ExpressBrute(store, {
-	freeRetries: 2
+    freeRetries: 2
 });
 
 const express = require('express');
 const app = express();
 app.get('/',
-	bruteforce.prevent, // 403 if we hit this route too often
-	function (req, res, next) {
-		res.send('Success!');
-	}
+    bruteforce.prevent, // 403 if we hit this route too often
+    function (req, res) {
+        res.send('Success!');
+    }
 );
 
 app.listen(3000);
